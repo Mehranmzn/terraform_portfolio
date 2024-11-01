f6cd188b0982921ff008d49aadf0f98711129f8f
Mehran Moazeni <moazeni.mehran@gmail.com>
2024-11-01 17:01:54 +0100

    provider example

--- 02-first-terraform-project/providers.tf
+++ 02-first-terraform-project/providers.tf
@@ -20,4 +20,15 @@ provider "aws" {
   
 }
 
+resource "random_id" "bucket_suffix" {
+    byte_length = 6
+}
 
+resource "aws_s3_bucket" "example_bucket" {
+    bucket = "example-bucket-${random_id.bucket_suffix.hex}"
+}
+
+output "bucket_name" {
+    value = aws_s3_bucket.example_bucket.bucket
+  
+}
